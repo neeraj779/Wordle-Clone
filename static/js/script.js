5733b504e62eeafc6a21b31b0382b7532a061910
Neeraj <neeraj79108@gmail.com>
2024-07-18 12:01:43 +0530

    chore: Update keyboard key label from "<<" to "DEL"

--- static/js/script.js
+++ static/js/script.js
@@ -94,7 +94,7 @@ function createKeyBoard() {
   const rows = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "<<"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"],
   ];
 
   const keyboardContainer = document.querySelector(".keyboard");
@@ -130,7 +130,7 @@ function getRandomInt(min, max) {
 function handleClick(key) {
   if (!isGameOver && !isAnimating) {
     switch (key) {
-      case "<<":
+      case "DEL":
         deleteLetter();
         break;
       case "BACKSPACE":
